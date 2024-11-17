615d8e1714cc108a50344df7a72e67ed0dd8219d
alex <alex@pop-os.hitronhub.home>
2024-11-17 14:45:03 -0500

    pokeapi, map, mapb

--- commands.go
+++ commands.go
@@ -2,21 +2,56 @@ package main
 import (
 	"fmt"
 	"os"
+	"errors"
 )
 
-func callbackHelp() error {
-	fmt.Println("Welcome to the Pokedex help menu!")
-	fmt.Println("Here are your available commands:")
-
-	availableCommands := getCommands()
-	for _, cmd := range availableCommands {
-		fmt.Printf(" - %s: %s\n", cmd.name, cmd.description)
-	}
-	fmt.Println("")
+func commandExit(cfg *config) error {
+	os.Exit(0)
 	return nil
 }
 
-func callbackExit() error{
-	os.Exit(0) // terminate the program without errors
+func commandHelp(cfg *config) error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func commandMapf(cfg *config) error {
+	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
+	if err != nil {
+		return err
+	}
+
+	cfg.nextLocationsURL = locationsResp.Next
+	cfg.prevLocationsURL = locationsResp.Previous
+
+	for _, loc := range locationsResp.Results {
+		fmt.Println(loc.Name)
+	}
+	return nil
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.prevLocationsURL == nil {
+		return errors.New("you're on the first page")
+	}
+
+	locationResp, err := cfg.pokeapiClient.ListLocations(cfg.prevLocationsURL)
+	if err != nil {
+		return err
+	}
+
+	cfg.nextLocationsURL = locationResp.Next
+	cfg.prevLocationsURL = locationResp.Previous
+
+	for _, loc := range locationResp.Results {
+		fmt.Println(loc.Name)
+	}
 	return nil
 }
--- repl.go
+++ repl.go
@@ -1,68 +1,80 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
+
+	"github.com/alexlangev/pokedexcli/internal/pokeapi"
 )
 
+type config struct {
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
+}
+
+func startRepl(cfg *config) {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback(cfg)
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
 type cliCommand struct {
-	name		string
-	description	string
-	callback func()
+	name        string
+	description string
+	callback    func(*config) error
 }
 
 func getCommands() map[string]cliCommand {
 	return map[string]cliCommand{
 		"help": {
-			name: "help",
-			description: "Prints the help menu",
-			callback: callbackHelp,
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
 		},
 		"exit": {
-			name: "exit",
-			description: "Turns off the Pokedex",
-			callback: callbackExit,
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
 		},
 	}
-}
-
-func startRepl() {
-	// Create scanner
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		// Prompt the user
-		fmt.Print(" >")
-		
-		// Read user prompt
-		scanner.Scan()
-		text := scanner.Text()
-		cleaned := cleanInput(text)
-
-		// Empty prompt (mashing return)
-		if len(cleaned) == 0 {
-			continue
-		}
-
-		commandName := cleaned[0]
-		availableCommands := getCommands()
-		command, ok := availableCommands[commandName]
-		if !ok {
-			fmt.Println("invalid command")
-			continue
-		}
-
-		command.callback()
-
-	}
-}
-
-
-// sanitize user input before eval
-func cleanInput(str string) []string {
-	lowered := strings.ToLower(str)
-	words := strings.Fields(lowered)
-	return words
 }
